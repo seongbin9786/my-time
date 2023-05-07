c4145173cd8c55b012e1b7f2e3e4d6429d99a183
Seongbin Kim <seongbin9786@gmail.com>
2023-05-07 17:22:06 +0900

    feat: 몰입 현황 그래프 컴포넌트, 더미 데이터

---
+++ src/components/charts/FlowStatusChart.tsx
@@ -0,0 +1,102 @@
+import {
+  Bar,
+  CartesianGrid,
+  ComposedChart,
+  Legend,
+  Line,
+  ReferenceLine,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+
+interface FlowStatusChartProps {
+  data: {
+    name: string;
+    giveup: number;
+    boring: number;
+    tryAgain: number;
+    totalSurrender: number;
+    totalTryAgain: number;
+  }[];
+  ratio: {
+    totalSurrenderRatio: number;
+    actualSurrenderRatio: number;
+    totalTryAgainRatio: number;
+  };
+}
+
+export const FlowStatusChart = ({ data, ratio }: FlowStatusChartProps) => {
+  const lastMinute = data[data.length - 1];
+  const { totalSurrender, totalTryAgain } = lastMinute;
+  const actualSurrender = totalSurrender + totalTryAgain;
+  const { totalSurrenderRatio, actualSurrenderRatio, totalTryAgainRatio } =
+    ratio;
+
+  return (
+    <div
+      style={{
+        width: '50vh',
+        height: '40vh',
+      }}
+    >
+      <div style={{ display: 'flex', gap: 4 }}>
+        <span style={{ color: 'blue' }}>
+          포기: {totalSurrender} (상위 {totalSurrenderRatio}%)
+        </span>
+        <span style={{ color: 'red' }}>
+          실질 포기: {actualSurrender} (상위 {actualSurrenderRatio}%)
+        </span>
+        <span style={{ color: 'green' }}>
+          재시도: {totalTryAgain * -1} (상위 {totalTryAgainRatio}%)
+        </span>
+      </div>
+      <ResponsiveContainer width="100%" height="100%">
+        <ComposedChart
+          stackOffset="sign"
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <ReferenceLine y={0} stroke="#000" />
+          <Bar stackId="1" dataKey="giveup" name="어려움" fill="red" />
+          <Bar stackId="1" dataKey="boring" name="지루함" fill="gray" />
+          <Line
+            type="monotone"
+            dataKey="totalSurrender"
+            name="누적 포기"
+            stroke="orange"
+            dot={false}
+          />
+          <Line
+            type="monotone"
+            dataKey={(o) => o.totalSurrender + o.totalTryAgain}
+            name="누적 실질 포기"
+            stroke="red"
+            dot={false}
+          />
+          <Bar stackId="1" dataKey="tryAgain" name="재시도" fill="green" />
+          <Line
+            type="monotone"
+            dataKey="totalTryAgain"
+            name="누적 재시도"
+            stroke="green"
+            dot={false}
+          />
+        </ComposedChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <div />
+import { FLOW_EXAPLE_DATA, FLOW_EXAPLE_RATIO } from './assets/api/FlowData';
+import { FlowStatusChart } from './components/charts/FlowStatusChart';
+
+const App = () => (
+  <div
+    style={{
+      width: '50vh',
+      height: '40vh',
+    }}
+  >
+    <h1>[몰입 모니터링]</h1>
+    <h2>"난도를 낮추면 더 재밌을 것 같다"</h2>
+    <FlowStatusChart data={FLOW_EXAPLE_DATA} ratio={FLOW_EXAPLE_RATIO} />
+  </div>
 );
+
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
