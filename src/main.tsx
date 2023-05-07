c326fb22ae6b38a4bfa736add100f9cbb2b4503a
Seongbin Kim <seongbin9786@gmail.com>
2023-05-07 22:03:28 +0900

    feat: 초과 휴식 시간 차트, 더미 데이터

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,35 @@
 import { createRoot } from 'react-dom/client';
 
+import { EXAMPLE_AVAILABLE_REST_TIME } from './assets/api/AvailableRestTimeData';
 import { FLOW_EXAPLE_DATA, FLOW_EXAPLE_RATIO } from './assets/api/FlowData';
+import { AvailableRestTimeChart } from './components/charts/AvailableRestTimeChart';
 import { FlowStatusChart } from './components/charts/FlowStatusChart';
 
 const App = () => (
   <div
     style={{
-      width: '50vh',
-      height: '40vh',
+      display: 'flex',
     }}
   >
-    <h1>[몰입 모니터링]</h1>
-    <h2>"난도를 낮추면 더 재밌을 것 같다"</h2>
-    <FlowStatusChart data={FLOW_EXAPLE_DATA} ratio={FLOW_EXAPLE_RATIO} />
+    <div
+      style={{
+        width: '50vh',
+        height: '40vh',
+      }}
+    >
+      <h1>[몰입 모니터링]</h1>
+      <h2>"난도를 낮추면 더 재밌을 것 같다"</h2>
+      <FlowStatusChart data={FLOW_EXAPLE_DATA} ratio={FLOW_EXAPLE_RATIO} />
+    </div>
+    <div
+      style={{
+        width: '50vh',
+        height: '40vh',
+      }}
+    >
+      <h1>[초과 휴식 시간]</h1>
+      <AvailableRestTimeChart logs={EXAMPLE_AVAILABLE_REST_TIME} />
+    </div>
   </div>
 );
 
