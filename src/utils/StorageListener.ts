9810eaa391ba3fb15b984f38f18c95906c68fe5c
Seongbin Kim <seongbin9786@gmail.com>
2023-08-22 19:42:39 +0900

    refactor(StorageListener): parameter 잘못된 이름 수정

--- src/utils/StorageListener.ts
+++ src/utils/StorageListener.ts
@@ -7,25 +7,25 @@ const STORAGE_EVENT = 'storage';
 export class StorageListener {
   private eventListener: StorageEventListener = null;
 
-  install(targetKey: string, onRemoteChange: StorageEventCallback) {
+  install(targetKey: string, callback: StorageEventCallback) {
     // 제거
     if (this.eventListener) {
       window.removeEventListener(STORAGE_EVENT, this.eventListener);
     }
 
     // 등록
-    const listener = this.create(targetKey, onRemoteChange);
+    const listener = this.create(targetKey, callback);
     window.addEventListener(STORAGE_EVENT, listener);
     this.eventListener = listener;
   }
 
   // 단순 생성
-  private create(targetKey: string, onRemoteChange: StorageEventCallback) {
+  private create(targetKey: string, callback: StorageEventCallback) {
     const listener = (e: StorageEvent) => {
       const { key, newValue } = e;
       // 동일 key에 대해서만 처리
       if (key === targetKey) {
-        onRemoteChange(newValue ?? '');
+        callback(newValue ?? '');
       }
     };
 
