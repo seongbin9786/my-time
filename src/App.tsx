e2f8e33fa1229474611c0c03d9a5f86eba533af8
Seongbin Kim <seongbin9786@gmail.com>
2023-08-23 19:38:13 +0900

    chore(라우팅 규칙): 기록지, 일 별 요약 페이지 라우팅 규칙 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
 import './App.css';
 
-import { useSelector } from 'react-redux';
+import { Routes } from './Routes';
 
-import { TextLogContainer } from './components/texts/TextLogContainer';
-import { Area_AvailableRestTimeChart } from './features/AvailableRestTimeChartArea';
-import { Area_ProductivePaceChart } from './features/ProductivePaceChartArea';
-import { RootState } from './store';
-
-export const App = () => {
-  // 바로 다음 컴포넌트이니 직접 주입, redux 의존성 낮추기 위함.
-  const logsForCharts = useSelector(
-    (state: RootState) => state.logs.logsForCharts
-  );
-
-  return (
-    <div className="min-w-[400px] h-screen max-w-screen-xl grid-cols-2 grid-rows-2 p-4 mx-auto my-0 sm:grid">
-      <TextLogContainer />
-      <Area_AvailableRestTimeChart logsForCharts={logsForCharts} />
-      <Area_ProductivePaceChart logsForCharts={logsForCharts} />
-    </div>
-  );
-};
+export const App = () => <Routes />;
